09c810ac727b3708a8eb87faf1fba6d1f43028aa
Elmar Amanov <Elmar.amanov10@gmail.com>
2022-05-30 07:06:21 +0400

    todo items variable

--- todo-app/src/app/todo/todo.component.ts
+++ todo-app/src/app/todo/todo.component.ts
@@ -1,12 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+interface ITODO_ITEMS {
+  id: number;
+  description: string;
+  action: string;
+}
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.scss']
 })
 export class TodoComponent implements OnInit {
-
+  private name: string = "Elmar";
+  
+  public get getName() : string {
+    return this.name; 
+  }
+  items: ITODO_ITEMS[] = [
+    {id: 1 , description: 'Seher Yemeyi', action: 'he'},
+    {id: 2 , description: 'Seher Bazarliqi', action: 'he'},
+    {id: 3 , description: 'Seher Idmani', action: 'yox'}
+  ]
   constructor() { }
 
   ngOnInit(): void {
